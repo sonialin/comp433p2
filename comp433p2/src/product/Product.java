c8240803cee0dc44fc167c58eefe0343e931f1c5
tingLIU66 <tliu6@luc.edu>
2016-09-30 00:48:23 -0500

    modify Product.java

--- comp433p2/src/product/Product.java
+++ comp433p2/src/product/Product.java
@@ -1,7 +1,25 @@
 package product;
 
 public class Product {
+	
 	int productID;
+	String productname;
+	String productdecription;
+	float productprice;
+	
+	public Product(	int productID, String productname, String productdecription,float productprice){
+		this.productID = productID;
+		this.productname = productname;
+		this.productdecription = productdecription;
+		this.productprice = productprice;
+		
+	}
+	
+	public void getProductInfo(){
+		//TO DO
+		//mySQL operation
+	}
+	
 
 }
 
