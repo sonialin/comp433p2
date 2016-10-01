1486c2661a4e864308c8e4a2234b2568b1cfd52f
Sonia Lin <sonia_lin@hotmail.com>
2016-10-01 18:47:20 -0500

    Added jdbc driver, db setup file, and test file for jdbc connection

---
+++ src/JdbcTest.java
@@ -0,0 +1,46 @@
+import java.sql.*;
+
+public class JdbcTest {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection myConn = null;
+		Statement myStmt = null;
+		ResultSet myRs = null;
+		
+		try {
+			// 1. Get a connection to database
+			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce?useSSL=false", "root" , "123456");
+			
+			System.out.println("Database connection successful!\n");
+			
+			// 2. Create a statement
+			myStmt = myConn.createStatement();
+			
+			// 3. Execute SQL query
+			myRs = myStmt.executeQuery("select * from Product");
+			
+			// 4. Process the result set
+			while (myRs.next()) {
+				System.out.println(myRs.getString("ProductName") + ", " + myRs.getString("ProductPrice"));
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();
+			}
+		}
+	}
+
+}
