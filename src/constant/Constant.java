608c966b6eb8c778a7d0217683a3be6669396576
tingLIU66 <tliu6@luc.edu>
2016-10-02 17:37:24 -0500

    modified

--- src/constant/Constant.java
+++ src/constant/Constant.java
@@ -2,8 +2,8 @@ package constant;
 
 public class Constant {
 	public static final String URL = "jdbc:mysql://localhost:3306/ecommerce";
-	public static final String USERNAME = "DBuser";
-	public static final String PASSWORD = "DBkaimen";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "123456";
 	//public static final String URL = null;
 	
 }
